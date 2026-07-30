5919272481a49e88ba8454038abb6c0e902ca054
Aditya Pratap Singh <adityapratapsingh728@gmail.com>
2026-07-31 00:13:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (35.29%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int minimumPushes(char* word) {
+    int n = strlen(word);
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        ans += (i / 8) + 1;
+    }
+
+    return ans;
+}
