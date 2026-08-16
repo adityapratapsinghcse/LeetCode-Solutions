288967ea085abe33d7fe047047ac2178598ec25a
Aditya Pratap Singh <adityapratapsingh728@gmail.com>
2026-08-16 15:51:01 +0530

    Time: 3 ms (59.92%), Space: 20.4 MB (74.72%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = [0] * (n + 1)
+
+        for num in nums:
+            freq[num] +=1
+        
+        for i in range(n+1):
+            if(freq[i] == 0):
+                return i
