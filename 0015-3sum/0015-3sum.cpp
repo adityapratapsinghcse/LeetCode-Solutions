2b9314c68f4d7c130811fe6ec4fc0a806f360356
Aditya Pratap Singh <adityapratapsingh728@gmail.com>
2026-08-18 21:30:34 +0530

    Time: 47 ms (63.87%), Space: 29.2 MB (28.89%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -16,15 +16,12 @@ public:
 
                 if(sum == 0){
                     ans.push_back({nums[i] , nums[left] ,nums[right]});
-                    left++;
-                    right--;
-                    // Skip duplicate left
-                    while(left < right && nums[left] == nums[left - 1])
-                        left++;
 
-                    // Skip duplicate right
-                    while(left < right && nums[right] == nums[right + 1])
-                        right--;
+                    while (left<right && nums[left] == nums[left+1]){left++;}
+                    while(left<right && nums[right] == nums[right-1]){right--;}
+
+                    left++;;
+                    right--;
                 }
                 else if(sum > 0){
                     right--;
