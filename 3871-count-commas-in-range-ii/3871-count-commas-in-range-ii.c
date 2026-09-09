334afbbf04b97a0dd96b538cd89c3645d39ec407
Aditya Pratap Singh <adityapratapsingh728@gmail.com>
2026-09-09 23:43:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (21.05%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.c
@@ -0,0 +1,11 @@
+long long countCommas(long long n) {
+    long long ans = 0;
+        long long power = 1000;
+
+        while (power <= n) {
+            ans += n - power + 1;
+            power *= 1000;
+        }
+
+        return ans;
+}
